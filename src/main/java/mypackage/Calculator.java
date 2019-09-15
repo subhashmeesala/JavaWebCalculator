cf42e179b7608d82ceb021fe3df74b6de1dc10c0
Subbarao Meesala <submeesa@in.ibm.com>
2019-09-16 01:02:58 +0530

    correction1

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -16,7 +16,7 @@ public class Calculator extends HttpServlet
 		return second-first;
 	}
 	
-	public long div (long first, long second){
+	public long divfucn(long first, long second){
 		
 		return first*second;
 	}
