c2ddb1e89be4ee64caf419b45a65cd8308c4f879
Subbarao Meesala <submeesa@in.ibm.com>
2019-09-16 17:03:12 +0530

    test

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -43,7 +43,7 @@ public long mulFucn(long first, long second){
         }
         if(request.getParameter("r3")!=null)
         {
-            out.println("<h1>Multiplication</h1>"+mulFucn(a1, a2));
+            out.println("<h1>My Multiplication</h1>"+mulFucn(a1, a2));
         }
         RequestDispatcher rd=request.getRequestDispatcher("/index.jsp");  
         rd.include(request, response);  
